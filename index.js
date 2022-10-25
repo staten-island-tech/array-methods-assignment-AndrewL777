2386681d4c945819c180a7f054bee19512a89a55
AndrewL777 <7landrew7@gmail.com>
2022-10-25 13:57:40 -0400

    10-25-22

---
+++ index.js
@@ -0,0 +1,11 @@
+const Jacob = ["hotdog", "burger", "fries"];
+
+Jacob.forEach((food) => console.log(food));
+
+const Kyle = ["rice", "noodles", "eggs"];
+
+Jacob.forEach((food2) => console.log(food2));
+
+const Nick = ["chicken", "beef", "pork"];
+
+Jacob.forEach((food3) => console.log(food3));
