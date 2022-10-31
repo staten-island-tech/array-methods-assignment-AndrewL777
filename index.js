b03e8eb11292e77c54fcb936090b044b2480ded7
AndrewL777 <7landrew7@gmail.com>
2022-10-31 13:58:17 -0400

    10-31-22

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
 const foods = [
   {
-    food: "",
+    food: "chicken",
+    tasty: "tasty",
+    cheap: "cheap",
+    vegan: "not vegan",
+  },
+  {
+    food: "lobster",
+    tasty: "tasty",
+    cheap: "expensive",
+    vegan: "not vegan",
+  },
+  {
+    food: "french fries",
+    tasty: "tasty",
+    cheap: "cheap",
+    vegan: " vegan",
+  },
+  {
+    food: "ice cream",
+    tasty: "tasty",
+    cheap: "cheap",
+    vegan: "not vegan",
+  },
+  {
+    food: "grapefruits",
+    tasty: "not tasty",
+    cheap: "cheap",
+    vegan: "vegan",
+  },
+  {
+    food: "wagyu beef",
+    tasty: "tasty",
+    cheap: "expensive",
+    vegan: "not vegan",
+  },
+  {
+    food: "noodles",
+    tasty: "tasty",
+    cheap: "cheap",
+    vegan: "vegan",
+  },
+  {
+    food: "eggs",
+    tasty: "tasty",
+    cheap: "cheap",
+    vegan: "not vegan",
+  },
+  {
+    food: "oysters",
+    tasty: "not tasty",
+    cheap: "expensive",
+    vegan: "not vegan",
   },
 ];
+foods.foreach((fd) => {
+  fd.food.forEach((foods) => {
+    console.log(foods);
+  });
+});
