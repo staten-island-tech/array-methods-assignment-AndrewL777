ab224ee27a7ab9bd4cf2ee3d9b89dbbf6c4472fe
AndrewL777 <7landrew7@gmail.com>
2022-11-02 13:59:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,79 @@
-const foods = [
+const people = [
   {
-    item: "chicken",
-    tasty: "tasty",
-    cheap: "cheap",
-    habitat: ["land"],
+    name: "John",
+    animal: "chicken",
+    color: "blue",
+    device: "ipad",
+    locker: ["142"],
   },
   {
-    item: "lobster",
-    tasty: "tasty",
-    cheap: "expensive",
-    habitat: ["sea"],
+    name: "Bob",
+    animal: "lobster",
+    color: "red",
+    device: "phone",
+    locker: ["53"],
   },
   {
-    item: "beef",
-    tasty: "tasty",
-    cheap: "cheap",
-    habitat: ["land"],
+    name: "Jake",
+    animal: "cow",
+    color: "phone",
+    device: "laptop",
+    locker: ["643"],
   },
   {
-    item: "fish",
-    tasty: "tasty",
-    cheap: "cheap",
-    habitat: ["sea"],
+    name: "Harry",
+    animal: "fish",
+    color: "orange",
+    device: "phone",
+    locker: ["1"],
   },
   {
-    item: "lamb",
-    tasty: "not tasty",
-    cheap: "cheap",
-    habitat: ["land"],
+    name: "Link",
+    animal: "lamb",
+    color: "green",
+    device: "laptop",
+    locker: ["2"],
   },
   {
-    item: "duck",
-    tasty: "tasty",
-    cheap: "cheap",
-    habitat: ["land,sea"],
+    name: "Anson",
+    animal: "duck",
+    color: "purple",
+    device: "phone",
+    locker: ["72"],
   },
   {
-    item: "crabs",
-    tasty: "tasty",
-    cheap: "cheap",
-    habitat: ["land,sea"],
+    name: "Jayson",
+    animal: "crab",
+    color: "orange",
+    device: "laptop",
+    locker: ["73"],
   },
   {
-    item: "shrimp",
-    tasty: "tasty",
-    cheap: "cheap",
-    habitat: ["sea"],
+    name: "Taylor",
+    animal: "shrimp",
+    color: "black",
+    device: "macbook",
+    locker: ["73"],
   },
   {
-    item: "oysters",
-    tasty: "not tasty",
-    cheap: "expensive",
-    habitat: ["land,sea"],
+    name: "Hunter",
+    animal: "oyster",
+    color: "white",
+    device: "ipad",
+    locker: ["13"],
   },
 ];
 
-foods.forEach((animal) => {
-  console.log(animal.item);
-  animal.habitat.forEach((list) => {
+people.forEach((person) => {
+  console.log(person.name);
+  person.locker.forEach((list) => {
     console.log(list);
   });
 });
+const result = people.filter(locknumber);
+{
+  function locknumber(locker) {
+    return locker >= 100;
+  }
+}
+console.log(result);
